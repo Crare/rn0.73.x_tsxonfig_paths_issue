1ac305e568885aa909a94a0d7d228f9b4c51c052
Juho Heikkinen <juho.heikkinen@zure.com>
2024-03-08 18:34:18 +0700

    changes

---
+++ ReproducerApp/components/TestComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+export const TestComponent = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
